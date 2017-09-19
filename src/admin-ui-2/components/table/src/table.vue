fe8f1081607cafe3edb338af8de0ef18219c3955
Awey <chenwei1@rongcapital.cn>
2017-09-19 21:42:11 +0800

    table comp finished

--- src/admin-ui-2/components/table/src/table.vue
+++ src/admin-ui-2/components/table/src/table.vue
@@ -1,11 +1,11 @@
 <style lang="scss">
   @import '../../../style/vars';
   .au-table {
-    margin: 8px 0;
+    // margin: 8px 0;
     // border-radius: 4px;
     // the border around table
     // every tr except the last one in tbody has a bottom line
-    & > tr,
+    & > tr:not(:last-child),
     tbody > tr,
     tfoot > tr:not(:last-child),
     thead > tr {
@@ -45,17 +45,26 @@
   }
   .au-bordered {
     // border: 1px solid $grayBrighten15;
+    border-width: 1px;
+    border-style: solid;
     th:not(:last-child), td:not(:last-child) {
       // border-right: 1px solid $grayBrighten15;
+      border-right-width: 1px;
+      border-right-style: solid;
     }
     thead > tr:last-child, tbody > tr:last-child {
       // border-bottom-color: $grayBrighten10;
+      border-bottom-width: 1px;
+      border-bottom-style: solid;
     }
   }
 </style>
 
 <template>
-  <table class="au-table" ref="table">
+  <table class="
+    au-table au-theme-border-color--base-8
+    au-theme-background-color--base-12"
+    :class="{ 'au-bordered' : bordered }" ref="table">
     <slot></slot>
   </table>
 </template>
@@ -66,18 +75,20 @@
   export default {
     name: 'au-table',
     props: {
-      // TODO:
+      striped: Boolean,
+      bordered: Boolean
     },
     mounted () {
       let table = this.$refs.table
 
-      // let thead = table.querySelectorAll('thead')
-      // let tbody = table.querySelectorAll('tbody')
-      // let tfoot = table.querySelectorAll('tfoot')
+      let thead = table.querySelectorAll('thead')
+      let tbody = table.querySelectorAll('tbody')
+      let tfoot = table.querySelectorAll('tfoot')
 
       // console.log(thead)
       let allTr = table.querySelectorAll('tr')
-      console.log(allTr)
+      let allTd = table.querySelectorAll('td')
+      let allTh = table.querySelectorAll('th')
       // let allTd = table.querySelectorAll('td')
 
       // let theadTr = thead.querySelectorAll('tr')
@@ -85,24 +96,57 @@
       // let tfootTr = tfoot.querySelectorAll('tr')
       for (let i = 0; i < allTr.length; i++) {
         addClass(allTr[i], 'au-theme-border-color--base-8')
-        console.log(allTr[i])
+      }
+      for (let i = 0; i < allTh.length; i++) {
+        addClass(allTh[i], 'au-theme-font-color--base-3')
+      }
+      for (let i = 0; i < allTd.length; i++) {
+        addClass(allTd[i], 'au-theme-font-color--base-3 au-theme-border-color--base-8')
       }
 
-      // if (tbody) {
-      //   for (let i = 0; i < theadTr.lenght; i++) {
-      //     addClass(theadTr, 'au-theme-border-color--base-8')
-      //   }
-      //   for (let i = 0; i < tbodyTr.lenght; i++) {
-      //     addClass(tbodyTr, 'au-theme-border-color--base-8')
-      //   }
-      //   for (let i = 0; i < tfootTr.lenght - 1; i++) {
-      //     addClass(tfootTr, 'au-theme-border-color--base-8')
-      //   }
-      // } else {
-      //   for (let i = 0; i < allTr.lenght - 1; i++) {
-      //     addClass(allTr, 'au-theme-border-color--base-8')
-      //   }
-      // }
+      for (let i = 0; i < tfoot.length; i++) {
+        let tds = tfoot[i].querySelectorAll('td')
+        for (let i = 0; i < tds.length; i++) {
+          addClass(tds[i], 'au-theme-font-color--base-6')
+        }
+      }
+
+      for (let i = 0; i < thead.length; i++) {
+        let ths = thead[i].querySelectorAll('th')
+        for (let i = 0; i < ths.length; i++) {
+          addClass(ths[i], 'au-theme-font-color--base-6 au-theme-border-color--base-8')
+        }
+      }
+
+      if (tbody.length) {
+        for (let i = 0; i < tbody.length; i++) {
+          let trs = tbody[i].querySelectorAll('tr')
+          for (let i = 0; i < trs.length; i++) {
+            addClass(trs[i], 'au-theme-hover-background-color--base-10')
+          }
+          if (this.striped) {
+            for (let i = 0; i < trs.length; i++) {
+              if (i % 2 === 0) addClass(trs[i], 'au-theme-background-color--base-11')
+            }
+          }
+        }
+      } else {
+        for (let i = 0; i < allTr.length; i++) {
+          addClass(allTr[i], 'au-theme-hover-background-color--base-10')
+        }
+        if (this.striped) {
+          for (let i = 0; i < allTr.length; i++) {
+            if (i % 2 === 0) addClass(allTr[i], 'au-theme-background-color--base-11')
+          }
+        }
+      }
+
+      if (thead.length) {
+        addClass(thead[thead.length - 1].querySelector('tr:last-child'), 'au-theme-border-color--base-6-important')
+      }
+      if (tbody.length) {
+        addClass(tbody[thead.length - 1].querySelector('tr:last-child'), 'au-theme-border-color--base-6-important')
+      }
     }
   }
 </script>
--- src/examples/table-examples.vue
+++ src/examples/table-examples.vue
@@ -1,27 +1,136 @@
 <style lang="scss">
   .au-table-examples {
-
+    padding: 40px;
   }
 </style>
 
 <template>
   <div class="au-table-examples">
+    <au-table :striped="true">
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+      </tr>
+    </au-table>
+    <p>---------------</p>
     <au-table>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>2</td>
-        <td>3</td>
-      </tr>
+      <thead>
+        <tr>
+          <th>a</th>
+          <th>b</th>
+          <th>c</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>q</td>
+          <td>t</td>
+          <td>j</td>
+        </tr>
+      </tfoot>
+    </au-table>
+    <p>---------------</p>
+    <au-table :bordered="true" :striped="true">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>项目</th>
+          <th>发布时间</th>
+          <th>状态</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Cube</td>
+          <td>2016-12-14</td>
+          <td>已发布</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Stream</td>
+          <td>2017-03-15</td>
+          <td>开发中</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>Web2Data</td>
+          <td>2017-05-20</td>
+          <td>开发中</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>Admin System</td>
+          <td>2017-05-30</td>
+          <td>开发中</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="4">当前显示10行，共100行</td>
+        </tr>
+
+      </tfoot>
+    </au-table>
+    <p>---------------</p>
+    <au-table>
+      <thead>
+        <tr>
+          <th>a</th>
+          <th>b</th>
+          <th>c</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+        </tr>
+        <tr>
+          <td>7</td>
+          <td>8</td>
+          <td>9</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <td colspan="3">ds: 2</td>
+        </tr>
+      </tfoot>
     </au-table>
   </div>
 </template>

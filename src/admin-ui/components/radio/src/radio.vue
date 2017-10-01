870ac8207b0e7f42e31922e7207985772706205e
Awey <chenwei1@rongcapital.cn>
2017-10-01 18:20:18 +0800

    add warnings in checkbox

---
+++ src/admin-ui/components/radio/src/radio.vue
@@ -0,0 +1,18 @@
+<style lang="scss">
+  .au-radio {
+
+  }
+</style>
+<template>
+  <div class="au-radio"></div>
+</template>
+<script>
+  import
+
+  export default {
+    name: 'au-radio',
+
+  }
+</script>
+
+
---
+++ src/examples/checkbox.vue
@@ -0,0 +1,53 @@
+<style lang="scss" scoped>
+
+</style>
+<template>
+  <div class="page">
+    <au-panel>
+      <au-checkbox v-model="value" label="female" text="jazz"></au-checkbox><br>
+      <au-checkbox v-model="values" label="male" :checkboxes="checkboxes" disabled></au-checkbox><br>
+      <au-checkbox v-model="values" label="male" :checkboxes="checkboxes" :validators="validators"></au-checkbox><br>
+    </au-panel>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'checkbox-examples',
+    data () {
+      return {
+        value: false,
+        values: ['l'],
+        checkboxes: [
+          {
+            text: 'breaking',
+            value: 'b'
+          },
+          {
+            text: 'poppin',
+            value: 'p'
+          },
+          {
+            text: 'locking',
+            value: 'l'
+          }
+        ],
+        validators: [
+          {
+            validator (v) {
+              return v.includes('b')
+            },
+            warning: 'breaking forever'
+          },
+          {
+            validator (v) {
+              return !!v.length
+            },
+            warning: 'you need dance'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+

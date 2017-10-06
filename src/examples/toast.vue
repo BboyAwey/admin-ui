0a6bb0f7275635c874534dc9ec63ba748ed3ad7a
Awey <chenwei1@rongcapital.cn>
2017-10-06 17:18:05 +0800

    bug fixed in toast: if passed top or left then should not repos this direction

---
+++ src/examples/toast.vue
@@ -0,0 +1,33 @@
+<style lang="scss" scoped>
+
+</style>
+<template>
+  <div class="page">
+    <au-button @click="toast({
+      message: 'Toast Me!'
+    })">Toast me</au-button>
+    <au-button @click="toast({
+      message: 'Toast Me Too!',
+      icon: 'info-circle',
+      iconClass: 'au-theme-font-color--danger-3'
+    })">Toast me</au-button>
+    <au-button @click="toast({
+      message: 'Toast Me Too!',
+      icon: 'info-circle',
+      iconClass: 'au-theme-font-color--danger-3',
+      top: 100,
+      left: 100
+    })">Toast me</au-button>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'toast-examples',
+    methods: {
+      toast (config) {
+        this.$toast(config)
+      }
+    }
+  }
+</script>
+

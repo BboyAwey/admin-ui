c5426683df0cefb573e2a65a034ed9ed4a6e82e7
chenwei1 <chenwei1@rongcapital.cn>
2018-01-31 10:19:28 +0800

    yarn comand not correct

--- src/examples/installation.vue
+++ src/examples/installation.vue
@@ -5,7 +5,7 @@
         推荐的安装方式是使用yarn（npm或cnpm也可以，但命令略有区别）进行安装：
       </p>
       <code-h lang="ssh">
-        yarn add git+ssh://git@gogs.in.dataengine.com:Admin-UI/admin-ui-2-publish.git
+        yarn add admin-ui git+ssh://git@gogs.in.dataengine.com:Admin-UI/admin-ui-2-publish.git
       </code-h>
       <cite class="cite au-theme-before-background-color--base-8 au-theme-font-color--base-5">
         由于使用的是公司的私有代码库，请确保你的机器有权访问这个git地址。

5817f9aeebec5f15ed379a31204599a9d19ddc4a
chenwei1 <chenwei1@rongcapital.cn>
2018-03-08 10:43:25 +0800

    none

--- src/admin-ui/dist/theme.js
+++ src/admin-ui/dist/theme.js
@@ -652,48 +652,48 @@ module.exports = __webpack_require__("aLzV");
 /* harmony default export */ __webpack_exports__["a"] = ({
   colors: {
     'base-0': '#000',
-    'base-1': '#303030',
-    'base-2': '#444144',
-    'base-3': '#625f64 ',
-    'base-4': '#666666',
-    'base-5': '#a09da0',
-    'base-6': '#bebbbf',
-    'base-7': '#9e9e9e',
-    'base-8': '#dcdcdc',
-    'base-9': '#cccccc',
-    'base-10': '#ededed',
-    'base-11': '#f7f7f7',
+    'base-1': '#232a29',
+    'base-2': '#2f3938',
+    'base-3': '#44514f ',
+    'base-4': '#616e6c',
+    'base-5': '#7c8886',
+    'base-6': '#b1bbba',
+    'base-7': '#d9dedd',
+    'base-8': '#eaf0ef',
+    'base-9': '#f3f7f6',
+    'base-10': '#f7fcfb',
+    'base-11': '#fdfdfd',
     'base-12': '#fff',
 
-    'primary-1': '#002b23', // TODO:
+    'primary-1': '#01241d',
     'primary-2': '#3fd5b8',
     'primary-3': '#19bc9d',
     'primary-4': '#169f85',
-    'primary-5': '#e5fffa', // TODO:
+    'primary-5': '#e7f7f4',
 
-    'info-1': '#00243c', // TODO:
+    'info-1': '#011725',
     'info-2': '#f0faf8',
     'info-3': '#3498db',
     'info-4': '#2d82ba',
-    'info-5': '#c2e7ff', // TODO:
+    'info-5': '#e6f3fc',
 
-    'warning-1': '#2d1c00', // TODO:
+    'warning-1': '#251800',
     'warning-2': '#fec564',
     'warning-3': '#ffb433',
     'warning-4': '#e99b14',
-    'warning-5': '#ffeac5', // TODO:
+    'warning-5': '#fbf3e5',
 
-    'danger-1': '#2f0400', // TODO:
+    'danger-1': '#220401',
     'danger-2': '#f56354',
     'danger-3': '#e74c3c',
     'danger-4': '#c33a2c',
-    'danger-5': '#ffcbc6', // TODO:
+    'danger-5': '#fae7e5',
 
-    'success-1': '#002200', // TODO:
+    'success-1': '#012401',
     'success-2': '#7fcb7f',
     'success-3': '#5cb95c',
     'success-4': '#3da63d',
-    'success-5': '#daffda' // TODO:
+    'success-5': '#e7fae7'
   },
   shadows: {
     'level-1': '0 1px 4px rgba(0, 0, 0, .2)',

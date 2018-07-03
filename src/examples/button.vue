d43cc689c6590b47098c7c610eac5796e3323557
Awey <bboyawey@sina.cn>
2018-07-03 14:55:33 +0800

    remove the addition z-index to loading target parent el

--- src/examples/button.vue
+++ src/examples/button.vue
@@ -8,8 +8,8 @@
       <div class="component-example">
         <au-button type="primary" @click="loading = !loading">按钮</au-button>
         <au-button type="primary" :disabled="true">按钮</au-button>
-        <au-button type="danger" :loading="loading">按钮</au-button>
-        <au-button type="primary" plain :loading="loading">按钮</au-button>
+        <au-button style="z-index: 0" type="danger" :loading="loading">按钮</au-button>
+        <au-button style="z-index: 0" type="primary" plain :loading="loading">按钮</au-button>
       </div>
       <!-- 组件示例 -->
     </au-panel>

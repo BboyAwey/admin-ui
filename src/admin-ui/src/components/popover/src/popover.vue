8cc43e2093e507c7b8ae4a9996b89b2e8e2d419b
awey <bboyawey@sina.cn>
2020-07-04 12:15:09 +0800

    debug: popover sometimes lost it's refs

--- src/admin-ui/src/components/popover/src/popover.vue
+++ src/admin-ui/src/components/popover/src/popover.vue
@@ -293,126 +293,129 @@ export default {
       this.visible = false
     },
     calPos () {
-      let pop = this.$refs.pop
-      let target = this.getTarget()
-      let content = this.$refs.content
-      if (!target) return
+      console.log(this.$refs)
+      this.$nextTick(() => {
+        let pop = this.$refs.pop
+        let target = this.getTarget()
+        let content = this.$refs.content
+        if (!target) return
 
-      // let popElmSize = getElementSize(this.$slots.content[0].elm)
-      // this.$refs.pop.style.width = popElmSize.width + 'px'
-      // this.$refs.pop.style.height = popElmSize.height + 'px'
+        // let popElmSize = getElementSize(this.$slots.content[0].elm)
+        // this.$refs.pop.style.width = popElmSize.width + 'px'
+        // this.$refs.pop.style.height = popElmSize.height + 'px'
 
-      let keys = this.placement.split(/\s+/)
-      let xes = new Set(['top', 'bottom', 'left', 'right'])
-      let ys = new Set(['left', 'center', 'right', 'top', 'middle', 'bottom'])
-      if (keys.length !== 2 || !xes.has(keys[0]) || !ys.has(keys[1])) {
-        keys = ['top', 'center']
-      }
-      this.localPlacement = keys.join(' ')
+        let keys = this.placement.split(/\s+/)
+        let xes = new Set(['top', 'bottom', 'left', 'right'])
+        let ys = new Set(['left', 'center', 'right', 'top', 'middle', 'bottom'])
+        if (keys.length !== 2 || !xes.has(keys[0]) || !ys.has(keys[1])) {
+          keys = ['top', 'center']
+        }
+        this.localPlacement = keys.join(' ')
 
-      if (this.x && this.y) {
-        pop.style.left = this.x
-        pop.style.top = this.y
-        return
-      }
+        if (this.x && this.y) {
+          pop.style.left = this.x
+          pop.style.top = this.y
+          return
+        }
 
-      let targetSize = getElementSize(target)
-      let popSize = getElementSize(content)
-      let windowSize = {
-        width: document.body.clientWidth,
-        height: document.body.clientHeight
-      }
-      // handle screen overflow
-      let targetRect = target.getBoundingClientRect()
+        let targetSize = getElementSize(target)
+        let popSize = getElementSize(content)
+        let windowSize = {
+          width: document.body.clientWidth,
+          height: document.body.clientHeight
+        }
+        // handle screen overflow
+        let targetRect = target.getBoundingClientRect()
 
-      if (keys[0] === 'top') {
-        if (targetRect.top - 10 < popSize.height) keys[0] = 'bottom'
-      } else if (keys[0] === 'bottom') {
-        if (windowSize.height - targetRect.bottom - 10 < popSize.height) keys[0] = 'top'
-      } else if (keys[0] === 'left') {
-        if (targetRect.left - 10 < popSize.width) keys[0] = 'rigth'
-      } else if (keys[0] === 'right') {
-        if (windowSize.width - targetRect.right - 10 < popSize.width) keys[0] = 'left'
-      }
+        if (keys[0] === 'top') {
+          if (targetRect.top - 10 < popSize.height) keys[0] = 'bottom'
+        } else if (keys[0] === 'bottom') {
+          if (windowSize.height - targetRect.bottom - 10 < popSize.height) keys[0] = 'top'
+        } else if (keys[0] === 'left') {
+          if (targetRect.left - 10 < popSize.width) keys[0] = 'rigth'
+        } else if (keys[0] === 'right') {
+          if (windowSize.width - targetRect.right - 10 < popSize.width) keys[0] = 'left'
+        }
 
-      if (keys[0] === 'left' || keys[0] === 'right') {
-        if (keys[1] === 'bottom') {
-          if (targetRect.top - 10 < (popSize.height - targetSize.height)) keys[1] = 'top'
-        } else if (keys[1] === 'top') {
-          if (windowSize.height - targetRect.bottom - 10 < (popSize.height - targetSize.height)) keys[1] = 'bottom'
-        } else if (keys[1] === 'middle') {
-          let bottomSpace = windowSize.height - targetRect.bottom - 10
-          let topSpace = targetRect.top - 10
-          let halfSize = popSize.height / 2 - targetSize.height
-          let min = Math.min(bottomSpace, topSpace, halfSize)
-          let max = Math.max(bottomSpace, topSpace, halfSize)
-          if (min !== halfSize && max !== halfSize) {
-            if (min === topSpace) keys[1] = 'top'
-            else keys[1] = 'bottom'
+        if (keys[0] === 'left' || keys[0] === 'right') {
+          if (keys[1] === 'bottom') {
+            if (targetRect.top - 10 < (popSize.height - targetSize.height)) keys[1] = 'top'
+          } else if (keys[1] === 'top') {
+            if (windowSize.height - targetRect.bottom - 10 < (popSize.height - targetSize.height)) keys[1] = 'bottom'
+          } else if (keys[1] === 'middle') {
+            let bottomSpace = windowSize.height - targetRect.bottom - 10
+            let topSpace = targetRect.top - 10
+            let halfSize = popSize.height / 2 - targetSize.height
+            let min = Math.min(bottomSpace, topSpace, halfSize)
+            let max = Math.max(bottomSpace, topSpace, halfSize)
+            if (min !== halfSize && max !== halfSize) {
+              if (min === topSpace) keys[1] = 'top'
+              else keys[1] = 'bottom'
+            }
           }
         }
-      }
 
-      if (keys[0] === 'top' || keys[0] === 'bottom') {
-        if (keys[1] === 'right') {
-          if (targetRect.left - 10 < (popSize.width / (keys[1] === 'center' ? 2 : 1) - targetSize.width)) keys[1] = 'left'
-        } else if (keys[1] === 'left') {
-          if (windowSize.width - targetRect.right - 10 < (popSize.width - targetSize.width)) keys[1] = 'right'
-        } else if (keys[1] === 'center') {
-          let leftSpace = targetRect.left - 10
-          let rightSpace = windowSize.width - targetRect.right - 10
-          let halfSize = popSize.width / 2 - targetSize.width
-          let min = Math.min(leftSpace, rightSpace, halfSize)
-          let max = Math.max(leftSpace, rightSpace, halfSize)
-          if (min !== halfSize && max !== halfSize) {
-            if (min === leftSpace) keys[1] = 'left'
-            else keys[1] = 'right'
+        if (keys[0] === 'top' || keys[0] === 'bottom') {
+          if (keys[1] === 'right') {
+            if (targetRect.left - 10 < (popSize.width / (keys[1] === 'center' ? 2 : 1) - targetSize.width)) keys[1] = 'left'
+          } else if (keys[1] === 'left') {
+            if (windowSize.width - targetRect.right - 10 < (popSize.width - targetSize.width)) keys[1] = 'right'
+          } else if (keys[1] === 'center') {
+            let leftSpace = targetRect.left - 10
+            let rightSpace = windowSize.width - targetRect.right - 10
+            let halfSize = popSize.width / 2 - targetSize.width
+            let min = Math.min(leftSpace, rightSpace, halfSize)
+            let max = Math.max(leftSpace, rightSpace, halfSize)
+            if (min !== halfSize && max !== halfSize) {
+              if (min === leftSpace) keys[1] = 'left'
+              else keys[1] = 'right'
+            }
           }
         }
-      }
 
-      this.localPlacement = keys.join(' ')
+        this.localPlacement = keys.join(' ')
 
-      // fix the size bug witch caused by the wordwrap
-      // this.$refs.pop.style.width = popSize.width + 'px'
-      // this.$refs.pop.style.height = popSize.height + 'px'
-      let offset = 10
-      let vertical = {
-        x: {
-          left: targetRect.left + parseInt(this.xFix),
-          center: targetRect.left + targetSize.width / 2 - popSize.width / 2 + parseInt(this.xFix),
-          right: targetRect.left + targetSize.width - popSize.width + parseInt(this.xFix)
-        },
-        y: {
-          top: targetRect.top - popSize.height - offset + parseInt(this.yFix),
-          bottom: targetRect.top + targetSize.height + offset + parseInt(this.yFix) // do not kown why should add 10 but it works
+        // fix the size bug witch caused by the wordwrap
+        // this.$refs.pop.style.width = popSize.width + 'px'
+        // this.$refs.pop.style.height = popSize.height + 'px'
+        let offset = 10
+        let vertical = {
+          x: {
+            left: targetRect.left + parseInt(this.xFix),
+            center: targetRect.left + targetSize.width / 2 - popSize.width / 2 + parseInt(this.xFix),
+            right: targetRect.left + targetSize.width - popSize.width + parseInt(this.xFix)
+          },
+          y: {
+            top: targetRect.top - popSize.height - offset + parseInt(this.yFix),
+            bottom: targetRect.top + targetSize.height + offset + parseInt(this.yFix) // do not kown why should add 10 but it works
+          }
         }
-      }
-      let horizontal = {
-        x: {
-          left: targetRect.left - offset - popSize.width + parseInt(this.xFix),
-          right: targetRect.left + targetSize.width + offset + parseInt(this.xFix)
-        },
-        y: {
-          top: targetRect.top + parseInt(this.yFix),
-          middle: targetRect.top + targetSize.height / 2 - popSize.height / 2 + parseInt(this.yFix),
-          bottom: targetRect.top + targetSize.height - popSize.height + 11 + parseInt(this.yFix) // do not kown why should add 11 but it works
+        let horizontal = {
+          x: {
+            left: targetRect.left - offset - popSize.width + parseInt(this.xFix),
+            right: targetRect.left + targetSize.width + offset + parseInt(this.xFix)
+          },
+          y: {
+            top: targetRect.top + parseInt(this.yFix),
+            middle: targetRect.top + targetSize.height / 2 - popSize.height / 2 + parseInt(this.yFix),
+            bottom: targetRect.top + targetSize.height - popSize.height + 11 + parseInt(this.yFix) // do not kown why should add 11 but it works
+          }
         }
-      }
-      let res = {}
-      if (keys[0] === 'top' || keys[0] === 'bottom') {
-        res = {
-          x: vertical.x[keys[1]],
-          y: vertical.y[keys[0]]
+        let res = {}
+        if (keys[0] === 'top' || keys[0] === 'bottom') {
+          res = {
+            x: vertical.x[keys[1]],
+            y: vertical.y[keys[0]]
+          }
+        } else if (keys[0] === 'left' || keys[0] === 'right') {
+          res = {
+            x: horizontal.x[keys[0]],
+            y: horizontal.y[keys[1]]
+          }
         }
-      } else if (keys[0] === 'left' || keys[0] === 'right') {
-        res = {
-          x: horizontal.x[keys[0]],
-          y: horizontal.y[keys[1]]
-        }
-      }
-      pop.style.left = this.x || res.x + 'px'
-      pop.style.top = this.y || res.y + 'px'
+        pop.style.left = this.x || res.x + 'px'
+        pop.style.top = this.y || res.y + 'px'
+      })
     },
     handleWindowClick (e) {
       if (this.trigger === 'click' &&
